3025d2ee2cc830353f00abb8bf02f8ec7d7bdd87
Richard Potter <richardpotter.au@gmail.com>
2023-07-10 14:42:36 +1000

    style list

--- src/components/SignUpUpdateList.js
+++ src/components/SignUpUpdateList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import SignUpUpdateItem from "./SignUpUpdateItem";
 
+import classes from "./SignUpUpdateList.module.css";
+
 function SignUpUpdateList({ items }) {
   const itemComponents = items.map((item) => <SignUpUpdateItem item={item} />);
-  return <ul>{itemComponents}</ul>;
+  return <ul className={classes.list}>{itemComponents}</ul>;
 }
 export default SignUpUpdateList;
