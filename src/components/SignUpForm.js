3d087c5cf64e0bf9e46a81af25d22df2545bd2a6
Richard Potter <richardpotter.au@gmail.com>
2023-07-10 11:17:08 +1000

    implemted sign up form

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUpForm() {
+  return (
+    <>
+      <label>Email Address</label>
+      <input type="email" />
+      <button>Subcribe to monthly newsletter</button>
+    </>
+  );
+}
+
+export default SignUpForm;
