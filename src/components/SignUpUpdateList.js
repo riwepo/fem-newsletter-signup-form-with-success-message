7ff7a5aa530c2c6be776a14cf024c1a48bbca125
Richard Potter <richardpotter.au@gmail.com>
2023-07-11 08:16:31 +1000

    add key to list items

--- src/components/SignUpUpdateList.js
+++ src/components/SignUpUpdateList.js
@@ -4,7 +4,9 @@ import SignUpUpdateItem from "./SignUpUpdateItem";
 import classes from "./SignUpUpdateList.module.css";
 
 function SignUpUpdateList({ items }) {
-  const itemComponents = items.map((item) => <SignUpUpdateItem item={item} />);
+  const itemComponents = items.map((item) => (
+    <SignUpUpdateItem item={item} key={item} />
+  ));
   return <ul className={classes.list}>{itemComponents}</ul>;
 }
 export default SignUpUpdateList;
