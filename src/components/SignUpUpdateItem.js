e4cc9934d367b2a85bc1798e06cd6e221d18e17c
Richard Potter <richardpotter.au@gmail.com>
2023-07-10 12:17:09 +1000

    made it a li, added icon

--- src/components/SignUpUpdateItem.js
+++ src/components/SignUpUpdateItem.js
@@ -1,7 +1,15 @@
 import React from "react";
+import listIcon from "../assets/images/icon-list.svg";
+
+import classes from "./SignUpUpdateItem.module.css";
 
 function SignUpUpdateItem({ item }) {
-  return <h2>{item}</h2>;
+  return (
+    <li className={classes.listItem}>
+      <img src={listIcon} alt="tick" />
+      <h2>{item}</h2>
+    </li>
+  );
 }
 
 export default SignUpUpdateItem;
