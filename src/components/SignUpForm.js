54296c2abc1971f359397035d73dac35ac177129
Richard Potter <richardpotter.au@gmail.com>
2023-07-10 12:16:24 +1000

    put in form element, added form attributes

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import classes from "./SignUpForm.module.css";
+
 function SignUpForm() {
   return (
-    <>
-      <label>Email Address</label>
-      <input type="email" />
+    <form className={classes.form}>
+      <label htmlFor="email">Email Address</label>
+      <input type="email" id="email" placeholder="email@company.com" />
       <button>Subcribe to monthly newsletter</button>
-    </>
+    </form>
   );
 }
 
