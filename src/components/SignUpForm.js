6ee716e8e83c3874f3b21a8ae873776d7539b96b
Richard Potter <richardpotter.au@gmail.com>
2023-07-11 10:58:23 +1000

    add color variables add styling to validate email add styling to disable button

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,13 +1,49 @@
 import React from "react";
+import { useState } from "react";
 
 import classes from "./SignUpForm.module.css";
 
 function SignUpForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailIsTouched, setEnteredEmailIsTouched] = useState(false);
+  const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false);
+
+  const emailInputBlurHandler = (event) => {
+    console.log("blur");
+    // note we need to use a local variable here
+    // the state won't be updated instantly
+    const enteredEmailLocal = event.target.value;
+    setEnteredEmail(enteredEmailLocal);
+    setEnteredEmailIsTouched(true);
+    if (enteredEmailLocal.trim() === "") {
+      setEnteredEmailIsValid(false);
+      return;
+    }
+    if (enteredEmailLocal.trim().indexOf("@") === -1) {
+      setEnteredEmailIsValid(false);
+      return;
+    }
+
+    setEnteredEmailIsValid(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const emailHasError = enteredEmailIsTouched && !enteredEmailIsValid;
+  const submitDisabled = !enteredEmailIsTouched || !enteredEmailIsValid;
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={formSubmissionHandler}>
       <label htmlFor="email">Email Address</label>
-      <input type="email" id="email" placeholder="email@company.com" />
-      <button>Subcribe to monthly newsletter</button>
+      <input
+        className={`${emailHasError ? classes.invalid : ""}`}
+        type="text"
+        id="email"
+        placeholder="email@company.com"
+        onBlur={emailInputBlurHandler}
+      />
+      <button disabled={submitDisabled}>Subcribe to monthly newsletter</button>
     </form>
   );
 }
