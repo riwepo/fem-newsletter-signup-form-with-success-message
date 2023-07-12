eadac1a2d4c33aa65df9724df0d1d30a1101d87a
Richard Potter <richardpotter.au@gmail.com>
2023-07-12 12:08:13 +1000

    must use class in order to privatise selector

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import classes from "./Button.module.css";
 
-function Button({ content, isDisabled }) {
-  return <button disabled={isDisabled}>{content}</button>;
+function Button({ content, isDisabled, onClick }) {
+  return (
+    <button className={classes.button} disabled={isDisabled} onClick={onClick}>
+      {content}
+    </button>
+  );
 }
 
 export default Button;
