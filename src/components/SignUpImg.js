1c1d2c28c4a205a2d303d880c6e2bd2315c97e9d
Richard Potter <richardpotter.au@gmail.com>
2023-07-12 15:18:05 +1000

    display alternate images mobile / desktop

--- src/components/SignUpImg.js
+++ src/components/SignUpImg.js
@@ -1,8 +1,24 @@
 import React from "react";
-import heroImage from "../assets/images/illustration-sign-up-mobile.svg";
+import mobileImage from "../assets/images/illustration-sign-up-mobile.svg";
+import desktopImage from "../assets/images/illustration-sign-up-desktop.svg";
+
+import classes from "./SignUpImg.module.css";
 
 function SignUpImg() {
-  return <img src={heroImage} alt="newsletter illustration" />;
+  return (
+    <>
+      <img
+        className={classes.mobileImg}
+        src={mobileImage}
+        alt="newsletter illustration"
+      />
+      <img
+        className={classes.desktopImg}
+        src={desktopImage}
+        alt="newsletter illustration"
+      />
+    </>
+  );
 }
 
 export default SignUpImg;
