2a9f19c5c26140543b3503b9d7fd6a5b5bd02bf0
Richard Potter <richardpotter.au@gmail.com>
2023-07-12 12:10:22 +1000

    format file

--- src/components/SignUpImg.js
+++ src/components/SignUpImg.js
@@ -4,4 +4,5 @@ import heroImage from "../assets/images/illustration-sign-up-mobile.svg";
 function SignUpImg() {
   return <img src={heroImage} alt="newsletter illustration" />;
 }
+
 export default SignUpImg;
